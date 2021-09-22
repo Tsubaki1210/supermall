615da3a8749de2a66926ea586bb06148235b7705
c0rn <c0rn1210@outlook.com>
2021-09-22 16:55:31 +0800

    init

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,41 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners" :key="item.index">
+        <a :href="item.link">
+          <img :src="item.imgae" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import { Swiper, SwiperItem } from "components/common/swiper";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    Swiper,
+    SwiperItem,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then((res) => {
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   },
 };
 </script>
